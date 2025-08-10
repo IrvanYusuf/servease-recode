f8cc0be1c238c8fac288800413fb511caf08ce47
irvan <irvanyusufcahyadi4@gmail.com>
2025-08-11 00:55:50 +0700

    make testing route

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 const connectDb = require("./src/database/db.js");
-const apiEndpoints = require("./src/routes/index.js");
+// const apiEndpoints = require("./src/routes/index.js");
 const cors = require("cors");
 const multerErrorHandler = require("./src/errors/multerError.js");
 const errorHandler = require("./src/errors/errorHandler.js");
 const { CONFIG } = require("./src/config/index.js");
+const ApiResponse = require("./src/utils/response.js");
 
 // dotenv.config();
 
@@ -21,7 +22,10 @@ connectDb();
 app.get("/", (req, res) => {
   res.send("halo");
 });
-app.use("/api", apiEndpoints);
+app.get("/testing", (req, res) => {
+  return ApiResponse.successResponse(res, "success testing");
+});
+// app.use("/api", apiEndpoints);
 app.use(errorHandler);
 app.use(multerErrorHandler);
 
